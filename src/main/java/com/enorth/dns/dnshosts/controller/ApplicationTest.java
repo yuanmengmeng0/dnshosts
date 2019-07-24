f0309d644574e190c5330340ab0f2aa29225ae2a
yuanmengmeng0 <ymm15763609368@163.com>
2019-07-24 10:54:18 +0800

    修改一下

---
+++ src/main/java/com/enorth/dns/dnshosts/controller/ApplicationTest.java
@@ -0,0 +1,46 @@
+package com.enorth.dns.dnshosts.controller;/*
+ * @author  Administrator
+ * @description:
+ * @date 2019/7/17
+ * */
+
+import cn.com.enorth.utility.AppBeans;
+import com.enorth.dns.dnshosts.consts.DBProgNameConst;
+import com.enorth.dns.dnshosts.consts.ParamConst;
+import com.enorth.dns.dnshosts.consts.SysParamInit;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+@Component
+public class ApplicationTest implements ApplicationRunner {
+    private static String DEFAULT_PROGNAME = "dnshosts";
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("============系统初始化开始=================");
+        ResourceBundle rb = ResourceBundle.getBundle("sensitive");
+        Enumeration<String> keys = rb.getKeys();
+        Set<String> keySet = new HashSet<>();
+        while (keys.hasMoreElements()) {
+            String key = keys.nextElement();
+            keySet.add(key);
+            if (key.equals(ParamConst.PROG_NAME)) {
+                DBProgNameConst.setPROGNAME(rb.getString(key));
+            }
+        }
+        DEFAULT_PROGNAME = DBProgNameConst.getPROGNAME();
+        for (String key : keySet) {
+            String value = rb.getString(key);
+            // 先灌global表
+//            AppBeans.getGlobalUtil().addParam(DEFAULT_PROGNAME, key, value);
+            // 再灌系统参数
+            SysParamInit.getInstance().putLocalParam(key, value);
+        }
+        System.out.println("=============结束=================");
+    }
+}
